7f99db65487d426325fc7bfb0f46bc042b3c6684
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-02 22:28:11 +0500

    First React Native App

--- App.tsx
+++ App.tsx
@@ -1,12 +1,36 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {
+  View,
+  Text,
+  Image,
+  TouchableOpacity,
+  Alert,
+  Pressable,
+} from 'react-native';
 
 const App = () => {
   return (
     <View>
       <Text>App</Text>
+      <Image
+        style={{ width: 400, height: 300 }}
+        source={{
+          uri: 'https://images.unsplash.com/photo-1753808645481-070fba323120?q=80&w=875&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        }}
+      />
+      <TouchableOpacity
+        style={{ padding: 10, backgroundColor: 'green' }}
+        onPress={() => Alert.alert('Pressed!!')}
+      >
+        <Text>Button</Text>
+      </TouchableOpacity>
+      <Pressable
+        style={{ padding: 10, backgroundColor: 'yellow' }}
+        onLongPress={() => Alert.alert('Hello')}
+      >
+        <Text>Press Me!</Text>
+      </Pressable>
     </View>
-  )
-}
+  );
+};
 
-export default App
+export default App;
