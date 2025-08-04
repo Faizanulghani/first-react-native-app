eebf2c78c6928c4c04571b710c9c4cd95c852f03
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-04 19:00:02 +0500

    tabBar Navigation created with icons

--- App.tsx
+++ App.tsx
@@ -5,8 +5,13 @@ import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import Profile from './src/screens/Profile';
 import About from './src/screens/About';
 import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import AntDesign from 'react-native-vector-icons/dist/AntDesign';
+import EvilIcons from 'react-native-vector-icons/dist/EvilIcons';
+import AntDesign2 from 'react-native-vector-icons/dist/AntDesign';
 
 const Stack = createNativeStackNavigator();
+const Tab = createBottomTabNavigator();
 
 const App = () => {
   const StackNavigator = () => {
@@ -24,9 +29,51 @@ const App = () => {
       </Stack.Navigator>
     );
   };
+
+  function MyTabs() {
+    return (
+      <Tab.Navigator
+        screenOptions={{
+          tabBarActiveTintColor: 'red',
+          tabBarInactiveTintColor: 'black',
+          tabBarLabelStyle: { fontSize: 12 },
+          tabBarStyle: { height: 60 },
+        }}
+      >
+        <Tab.Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign name="home" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <EvilIcons name="user" size={size} color={color} />
+            ),
+          }}
+        />
+        <Tab.Screen
+          name="About"
+          component={About}
+          options={{
+            tabBarIcon: ({ color, size }) => (
+              <AntDesign2 name="profile" size={size} color={color} />
+            ),
+          }}
+        />
+      </Tab.Navigator>
+    );
+  }
   return (
     <NavigationContainer>
-      <StackNavigator />
+      {/* <StackNavigator /> */}
+      <MyTabs />
     </NavigationContainer>
   );
 };
