01c31e8e3147d7e9152a63f1bcf314d647baacdc
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-04 12:46:12 +0500

    Handling User Input

--- App.tsx
+++ App.tsx
@@ -1,164 +1,26 @@
-import { View, Image, Text, FlatList } from 'react-native';
+import { useState } from 'react';
+import { View, Image, Text, TextInput, Button } from 'react-native';
 
 const App = () => {
-  const dummy = [
-    {
-      id: 1,
-      name: 'Ali Raza',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 2,
-      name: 'Sara Khan',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 3,
-      name: 'Usman Tariq',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 4,
-      name: 'Ayesha Malik',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 5,
-      name: 'Zain Ahmed',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 6,
-      name: 'Hira Sheikh',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 7,
-      name: 'Bilal Aslam',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 8,
-      name: 'Mariam Iqbal',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 9,
-      name: 'Hamza Shahid',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 10,
-      name: 'Iqra Yousaf',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 11,
-      name: 'Farhan Ali',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 12,
-      name: 'Noor Fatima',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 13,
-      name: 'Tahir Javed',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 14,
-      name: 'Zoya Mirza',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 15,
-      name: 'Danish Khan',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 16,
-      name: 'Huma Nadeem',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 17,
-      name: 'Shayan Akram',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 18,
-      name: 'Nimra Abbas',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 19,
-      name: 'Faizan Qureshi',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 20,
-      name: 'Mehwish Rafiq',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 21,
-      name: 'Omer Aziz',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-    {
-      id: 22,
-      name: 'Rida Naeem',
-      image:
-        'https://images.unsplash.com/profile-1716316541074-00a45026d86e?w=150&dpr=1&crop=faces&bg=%23fff&h=150&auto=format&fit=crop&q=60&ixlib=rb-4.1.0',
-    },
-  ];
+  let [result, setResult] = useState('');
+  let [value, setValue] = useState('');
+
+  let handleSubmit = () => {
+    setResult(value);
+    setValue('');
+  };
 
   return (
     <View style={styles.container}>
-      <FlatList
-        data={dummy}
-        renderItem={({ item }) => (
-          <View style={styles.card}>
-            <Image
-              style={{ width: 48, height: 48, borderRadius: 50 }}
-              source={{
-                uri: item.image,
-              }}
-            />
-            <View style={{ alignItems: 'center' }}>
-              <Text>{item.name}</Text>
-              <Text>email</Text>
-            </View>
-          </View>
-        )}
-        keyExtractor={item => item.id}
-        ItemSeparatorComponent={<View style={{ height: 10 }} />}
-        numColumns={3}
-        columnWrapperStyle={{ gap: 10 }}
+      <Text style={styles.title}>Aur Bhai, KhushAmdeed hai apko</Text>
+      <TextInput
+        style={styles.input}
+        placeholder="Enter your name..."
+        value={value}
+        onChangeText={setValue}
       />
+      <Button title="Submit" onPress={handleSubmit}></Button>
+      {result ? <Text>Result: {result}</Text> : null}
     </View>
   );
 };
@@ -167,19 +29,22 @@ export default App;
 
 const styles = {
   container: {
-    width: '100%',
-    height: '100%',
-    backgroundColor: '#dadada',
-    paddingVertical: 10,
-    paddingHorizontal: 20,
-  },
-  card: {
-    width: 100,
-    height: 100,
-    backgroundColor: 'white',
-    borderRadius: 10,
-    padding: 5,
+    flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
+    padding: 20,
+    backgroundColor: '#dadada',
+    gap: 10,
+  },
+  title: {
+    fontSize: 20,
+    fontWeight: '500',
+  },
+  input: {
+    width: '100%',
+    padding: 5,
+    paddingVertical: 10,
+    borderWidth: 1,
+    borderRadius: 5,
   },
 };
