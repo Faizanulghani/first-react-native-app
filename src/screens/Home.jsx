e98b594babd2b4396048856bfe4ff7065f46bfb2
Faizan Ul Ghani <faizanulghani1@gmail.com>
2025-08-04 15:41:26 +0500

    sent data via prop in navigation

--- src/screens/Home.jsx
+++ src/screens/Home.jsx
@@ -1,14 +1,27 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-const Home = ({navigation}) => {
+const Home = ({ navigation }) => {
   return (
-    <View style={{width:'100%',height:'100%',justifyContent:'center',alignItems:'center',backgroundColor:'white'}}>
-      <Text style={{fontSize:20,fontWeight:500,marginBottom:10}}>Home</Text>
-      <Button title='Profile' onPress={()=>navigation.navigate('Profile')} />
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+      }}
+    >
+      <Text style={{ fontSize: 20, fontWeight: 500, marginBottom: 10 }}>
+        Home
+      </Text>
+      <Button
+        title="Profile"
+        onPress={() => navigation.navigate('Profile', { id: 1, name: 'john' })}
+      />
     </View>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
--- src/screens/Profile.jsx
+++ src/screens/Profile.jsx
@@ -1,14 +1,31 @@
-import { Button, StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native';
 
-const Profile = ({navigation}) => {
+const Profile = ({ navigation, route }) => {
+  const { id, name } = route.params;
   return (
-   <View style={{width:'100%',height:'100%',justifyContent:'center',alignItems:'center',backgroundColor:'white'}}>
-         <Text style={{fontSize:20,fontWeight:500,marginBottom:10}}>Profile</Text>
-         <Button title='About' onPress={()=>navigation.navigate('About')} />
-       </View>
-  )
-}
+    <View
+      style={{
+        width: '100%',
+        height: '100%',
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: 'white',
+      }}
+    >
+      <Text style={{ fontSize: 20, fontWeight: 500, marginBottom: 10 }}>
+        Profile
+      </Text>
+      <Text style={styles.text}>Id: {id} , Name: {name}</Text>
+      <Button title="About" onPress={() => navigation.navigate('About')} />
+    </View>
+  );
+};
 
-export default Profile
+export default Profile;
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+  text:{
+    fontSize: 20,
+    fontWeight: 500
+  }
+});
